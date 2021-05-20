f0a8e3b87867e931a3045779824ce1b12ee5dc64
thetevikrant <thetevikrant@gmail.com>
2021-05-20 15:09:35 +0530

    Option for highlighting anywhere in the text #5 (#6)
    * #5 option for highlighting anywhere in the text
    
    * removed setting of default value and updated test cases.
    
    * Update as per comment (https://github.com/moroshko/autosuggest-highlight/pull/6#discussion_r220165797)
    
    Co-authored-by: Vikrant Thete <>

--- src/match.test.js
+++ src/match.test.js
@@ -6,8 +6,12 @@ describe('match', function() {
     expect(match('some text', 'te')).to.deep.equal([[5, 7]]);
   });
 
-  it('should not highlight at the middle of a word', function() {
+  it('should not highlight at the middle of a word if third parameter is not passed or is set to false value', function() {
     expect(match('some text', 'e')).to.deep.equal([]);
+  });
+
+  it('should highlight at the middle of a word if passed third parameter as true', function() {
+    expect(match('some text', 'o', true)).to.deep.equal([[1, 2]]);
   });
 
   it('should highlight only the first match by default', function() {
